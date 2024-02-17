d2910d12ddafbff4a6911c4a95c290dfacbe0eec
Omar <omarcory2@gmail.com>
2024-02-16 19:30:20 -0600

    First commit

---
+++ src/main/java/Videojuegos.java
@@ -0,0 +1,9 @@
+
+public class Videojuegos {
+    public static void main(String[] args) {
+        String name = "Rocket League";
+        int rating = 9;
+        
+        System.out.println(rating);
+    }
+}
